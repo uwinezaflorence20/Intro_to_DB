314ccd526c60c192f20c51d10368d51110ff41e7
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-10-18 18:37:10 +0200

    corrected

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,14 @@
-import mysql.connector
-from mysql.connector import Error
 
+"""
+MySQLServer.py
+A simple Python script to create the 'alx_book_store' database.
+If the database already exists, it will not fail.
+"""
+
+import mysql.connector
 def create_database():
     try:
-        # Connect to MySQL server (adjust user/password/host if needed)
+
         connection = mysql.connector.connect(
             host='localhost',
             user='root',
@@ -15,14 +20,14 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
-            # print("MySQL connection is closed")  # Optional debug message
+    
 
 if __name__ == "__main__":
     create_database()

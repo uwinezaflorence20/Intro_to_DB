2113503ae51a5408871543d99277a69ecb6c3285
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-10-18 18:48:25 +0200

    another questions

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- task_4.sql
+-- Prints full description of the table 'Books' in alx_book_store
+
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() 
+  AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;

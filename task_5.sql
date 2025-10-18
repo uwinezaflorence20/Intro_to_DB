2c2460f4e2177c4cc84b004f35f2b47af20ac7d3
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-10-18 19:07:59 +0200

    another questions

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- task_5.sql
 -- Inserts a single row into the CUSTOMER table
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- task_6.sql
 -- Inserts multiple rows into the CUSTOMER table
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),

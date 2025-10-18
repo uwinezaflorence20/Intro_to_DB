117b31e60f7a5f2ce07c3c1c05aec808709fc623
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-10-18 18:53:38 +0200

    another questions

--- task_4.sql
+++ task_4.sql
@@ -2,13 +2,13 @@
 -- Prints full description of the table 'Books' in alx_book_store
 
 SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    COLUMN_NAME AS ColumnName,
+    COLUMN_TYPE AS ColumnType,
+    IS_NULLABLE AS IsNullable,
+    COLUMN_KEY AS ColumnKey,
+    COLUMN_DEFAULT AS DefaultValue,
+    EXTRA AS ExtraInfo
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() 
+WHERE TABLE_SCHEMA = DATABASE()
   AND TABLE_NAME = 'Books'
 ORDER BY ORDINAL_POSITION;
